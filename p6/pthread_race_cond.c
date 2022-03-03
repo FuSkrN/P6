b6e4a96c34b2f95611dc2a8b7e5c0bee04cd64d2
FuSkrN <madsbisgaardsc@gmail.com>
2022-03-03 09:30:11 +0100

    fixed c program for tests

--- p6/pthread_race_cond.c
+++ p6/pthread_race_cond.c
@@ -8,8 +8,8 @@ void *sumNumbers(void *);
 
 int main(){
     pthread_t thread1, thread2;
-    int arg1[] = {1, 1000000};
-    int arg2[] = {1000001, 2000000};
+    int arg1[] = {1, 1000};
+    int arg2[] = {1001, 2000};
     int iret1, iret2;
     pthread_t threads[10];
 
@@ -27,7 +27,7 @@ int main(){
         pthread_join(threads[x], NULL);
     }
 
-    printf("%d\n", &totalSum);
+    printf("totalSum at the end: %d\n", &totalSum);
 
 	return 0;
 }
@@ -38,53 +38,7 @@ void *sumNumbers(void *args){
     for(int x = arr[0]; x <= arr[1]; x++){
         sum += x;
     }
-    printf("%ld\n", &totalSum);
-    printf("%d", &sum);
+    printf("totalsum: %ld\n", &totalSum);
+    printf("sum: %d\n", &sum);
     totalSum += sum;
 }
-
-/*import threading
-n = 0
-
-class summationThread(threading.Thread):
-    def __init__(self, threadID, name, counter,\
-            sumRange):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-        self.sumRange = sumRange
-    def run(self):
-        #print("Starting " + self.name)
-        print(f'Thread {self.name} adding {sumNumbers(self.threadID, self.sumRange)} to sumArray[{self.threadID % 5}].')
-        #print("Exiting " + self.name)
-
-def sumNumbers(threadID, numberRange):
-    global sumArray
-    summedNumber = sumArray[threadID % 5]
-    for x in numberRange:
-        summedNumber += x
-    sumArray[threadID % 5] = summedNumber
-    return summedNumber
-
-#threads = []
-
-#sumArray = [0]*5
-
-#for x in range(1, 100):
-#    threads.append(summationThread(x, f"thread-{x}", x,\
-#            range((x-1)*1000000+1, x*1000000+1)))
-#
-#for thread in threads:
-#    thread.start()
-
-
-#for thread in threads:
-#    thread.join()
-#
-#for summation in sumArray:
-#    print(f"adding {summation} to n")
-#    n += summation
-
-#print(n)
-*/
