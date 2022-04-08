6f11cd19019cddb362fb03dea4baad299fe473ca
FuSkrN <madsbisgaardsc@gmail.com>
2022-04-08 13:53:22 +0200

    local_variables.c done

--- p6/local_variables.c
+++ p6/local_variables.c
@@ -2,20 +2,33 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *func(void *args);
+void *func();
 
 int main(void){
-    pthread_t thread1, thread2;
-    int res1, res2;
-    int arg1[] = {10};
+    pthread_t thread1;
+    pthread thread2;
+    int arg1[] = {};
     
     pthread_create(&thread1, NULL, func, (void*) arg1);
+    pthread_create(&thread2, NULL, func, (void*) arg1);
+
     pthread_join(thread1, NULL);
-    printf("%d\n", arg1[0]); 
+    pthread_join(thread2, NULL);
 }
 
-void *func(void *args){
-    int* a = (int*)args;
-    a += 10;
-    printf("its this one: %d\n", a);
+void *func(){
+    int a = 0;
+    int b = 10;
+    double x = 0.5;
+    y = 3.14;
+    
+    //random math
+    a = a + 7;
+    b = b + a;
+    a = b - a;
+    a = a + a;
+    b = b * a;
+
+    x = x * y;
+    y = x* x;
 }
--- p6/reducer.py
+++ p6/reducer.py
@@ -23,25 +23,25 @@ class reducer:
             stateQueue.pop(0)
 
 # Read and parse a C file, and then generate its resulting state array graph.
-a = python_reader.C_Reader('pthread_setting_variables.c')
-a.get_scopes(a.file)
-b = generator.graph(a.result)
+#a = python_reader.C_Reader('pthread_setting_variables.c')
+#a.get_scopes(a.file)
+#b = generator.graph(a.result)
 
 #Perform graph reduction.
-r = reducer(b.stateArray)
-r.reduce_sequence()
+#r = reducer(b.stateArray)
+#r.reduce_sequence()
 #for r in a.result:
 #    print(r)
-for x in r.stateArray:
-    print(f"stateName: {x.label}")
-    print(f"programCounters: {x.programCounters:}")
-    for p in x.symboltable.symboltable:
-        print(p)
-    for y in x.ingoing:
-        print(f"ingoing: {y.label}")
-    for z in x.outgoing:
-        print(f"outgoing: {z.label}")
-    print("\n")
+#for x in r.stateArray:
+#    print(f"stateName: {x.label}")
+#    print(f"programCounters: {x.programCounters:}")
+#    for p in x.symboltable.symboltable:
+#        print(p)
+#    for y in x.ingoing:
+#        print(f"ingoing: {y.label}")
+#    for z in x.outgoing:
+#        print(f"outgoing: {z.label}")
+#    print("\n")
 
-print(f"size before: {len(b.stateArray)}")
-print(f"size after: {len(r.stateArray)}")
+#print(f"size before: {len(b.stateArray)}")
+#print(f"size after: {len(r.stateArray)}")
